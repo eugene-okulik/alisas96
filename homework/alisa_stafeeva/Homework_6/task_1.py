a22b8f3ed5262aad402128a76ceb9201a22bf961
Alisas96 <alisas96@mail.com>
2025-10-21 19:05:47 +0300

    added homework 6

---
+++ homework/alisa_stafeeva/Homework_6/task_1.py
@@ -0,0 +1,12 @@
+text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+words = text.split()
+new_words = []
+for word in words:
+    if ',' in word:
+        word = word.replace(',', 'ing,')
+    elif '.' in word:
+        word = word.replace('.', 'ing.')
+    else:
+        word = word + 'ing'
+    new_words.append(word)
+print(' '.join(new_words))
