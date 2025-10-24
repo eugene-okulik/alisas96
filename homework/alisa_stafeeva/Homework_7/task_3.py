118d7db994271b7c4444ce88bcf27a16c1860766
Alisas96 <alisas96@mail.com>
2025-10-24 22:34:28 +0300

    lint errors fixed

--- homework/alisa_stafeeva/Homework_7/task_3.py
+++ homework/alisa_stafeeva/Homework_7/task_3.py
@@ -3,9 +3,11 @@ string_2 = 'результат операции: 54'
 string_3 = 'результат работы программы: 209'
 string_4 = 'результат: 2'
 
+
 def get_number(string):
     return int(string.split()[-1]) + 10
-    
+
+
 print(get_number(string_1))
 print(get_number(string_2))
 print(get_number(string_3))
