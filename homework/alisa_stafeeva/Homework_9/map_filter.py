c999f15125c47d2566650194a2ab994991bb2b38
Alisas96 <alisas96@mail.com>
2025-10-29 23:37:48 +0300

    lint error fixed

--- homework/alisa_stafeeva/Homework_9/map_filter.py
+++ homework/alisa_stafeeva/Homework_9/map_filter.py
@@ -1,4 +1,7 @@
-temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32, 30, 28, 24, 23]
+temperatures = [
+    20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27,
+    22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32, 30, 28, 24, 23
+]
 
 hot_temperatures = list(filter(lambda x: x > 28, temperatures))
 average_temperatures = sum(hot_temperatures) / len(hot_temperatures)
