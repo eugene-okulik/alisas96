cd48193758fc060946ff5cbf7a4c752b8a9c77c7
Alisas96 <alisas96@mail.com>
2025-12-01 13:03:23 +0300

    Func names changed, status fixed

--- homework/alisa_stafeeva/Homework_19/task.py
+++ homework/alisa_stafeeva/Homework_19/task.py
@@ -5,18 +5,18 @@ def clear(post_id):
     return requests.delete(f"http://objapi.course.qa-practice.com/object/{post_id}")
 
 
-def post_a_post():
+def post_an_object():
     body = {"name": "alisa_object_1", "data": {"color": "red", "size": "big"}}
     headers = {"Content-Type": "application/json"}
     response = requests.post(
         "http://objapi.course.qa-practice.com/object", json=body, headers=headers
     )
-    assert response.status_code == 201, "Status is incorrect"
-    assert response.json()["id"] == 581, "Id is incorrect"
+    assert response.status_code == 200, "Status is incorrect"
+    assert response.json()["name"] == "alisa_object_1", "Name is incorrect"
     clear(response.json()["id"])
 
 
-def new_post():
+def new_object():
     body = {"name": "alisa_object_2", "data": {"color": "black", "size": "giant"}}
     headers = {"Content-Type": "application/json"}
     response = requests.post(
@@ -25,8 +25,8 @@ def new_post():
     return response.json()["id"]
 
 
-def put_a_post():
-    post_id = new_post()
+def put_an_object():
+    post_id = new_object()
     body = {"name": "alisa_object_2", "data": {"color": "red", "size": "big"}}
     headers = {"Content-Type": "application/json"}
     response = requests.put(
@@ -39,8 +39,8 @@ def put_a_post():
     clear(post_id)
 
 
-def patch_a_post():
-    post_id = new_post()
+def patch_an_object():
+    post_id = new_object()
     body = {"data": {"color": "red", "size": "big"}}
     headers = {"Content-Type": "application/json"}
     response = requests.patch(
@@ -53,7 +53,7 @@ def patch_a_post():
     clear(post_id)
 
 
-def delete_a_post():
-    post_id = new_post()
+def delete_an_object():
+    post_id = new_object()
     response = requests.delete(f"http://objapi.course.qa-practice.com/object/{post_id}")
-    assert response.status_code == 204, "Delete failed"
+    assert response.status_code == 200, "Delete failed"
